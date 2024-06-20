4c4fe72538bb5eb8ff6e5d2e3cebb059e8c004b6
GuillermoFA <guillermo.fuentes01@alumnos.ucn.cl>
2024-06-20 15:14:52 -0400

    fix: error minor

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwind(), autoprefixer()],
-    },
-  },
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+ plugins: [vue()],
 })
