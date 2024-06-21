9cbd66e8275254b9dcc051d934d0aa3c1ea1b639
mizok <cairamgo@gmail.com>
2024-06-21 19:35:20 -0400

    feat: added workers table in companyview

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -3,18 +3,16 @@
     <TableCaption>A list of your recent invoices.</TableCaption>
     <TableHeader>
       <TableRow>
-        <TableHead class="w-[100px]"> Invoice </TableHead>
-        <TableHead>Status</TableHead>
-        <TableHead>Method</TableHead>
-        <TableHead class="text-right"> Amount </TableHead>
+        <TableHead v-for="col in columns" :key="col.name">{{
+          col.name
+        }}</TableHead>
       </TableRow>
     </TableHeader>
     <TableBody>
-      <TableRow>
-        <TableCell class="font-medium"> INV001 </TableCell>
-        <TableCell>Paid</TableCell>
-        <TableCell>Credit Card</TableCell>
-        <TableCell class="text-right"> $250.00 </TableCell>
+      <TableRow v-for="(row, rowIndex) in data" :key="rowIndex">
+        <TableCell v-for="(value, key) in row" :key="key">
+          {{ value }}
+        </TableCell>
       </TableRow>
     </TableBody>
   </Table>
@@ -30,4 +28,9 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+
+const props = defineProps({
+  data: Array,
+  columns: Array,
+});
 </script>
