f265962b7d1160b1b5d1b8edfc495c64e76fd95b
GuillermoFA <guillermo.fuentes01@alumnos.ucn.cl>
2024-06-21 23:38:47 -0400

    feat: card interventions

--- src/components/InterventionCard.vue
+++ src/components/InterventionCard.vue
@@ -1,20 +1,21 @@
 <template>
   <Card class="grid w-[250px] xl:w-[380px] max-h-[300px] min-h-[300px]">
     <CardHeader>
-      <CardTitle>{{ props.data.type }}</CardTitle>
-      <CardDescription>{{ props.data.details }}</CardDescription>
+      <CardTitle>{{ data.name }}</CardTitle>
+      <CardDescription>{{ data.duration }} Días</CardDescription>
+      <CardDescription>{{ data.description }}</CardDescription>
     </CardHeader>
     <CardContent class="grid gap-4">
       <div>
         <div
-          v-for="(competency, index) in props.data.intervened_competency"
+          v-for="(competency, index) in stringToArray(data.intervened_competencies)"
           :key="index"
           class="mb-4 grid grid-cols-[25px_minmax(0,1fr)] items-start pb-4 last:mb-0 last:pb-0"
         >
           <span class="flex h-2 w-2 translate-y-1 rounded-full bg-green-500" />
           <div class="space-y-1">
             <p class="text-sm font-medium leading-none">
-              {{ tranlasteCompetency(competency) }}
+              {{ translateCompetency(competency) }}
             </p>
           </div>
         </div>
@@ -41,10 +42,20 @@ import {
 } from "@/components/ui/card";
 
 const props = defineProps({
-  data: Object,
+  data: {
+    type: Object,
+    required: true,
+  },
 });
 
-const tranlasteCompetency = (competency) => {
+console.log(props.data);
+
+const stringToArray = (string) => {
+  return string.split(",");
+};
+
+const translateCompetency = (competency) => {
+  console.log(competency);
   switch (competency) {
     case "adaptability_to_change":
       return "Adaptabilidad al Cambio";
@@ -62,4 +73,6 @@ const tranlasteCompetency = (competency) => {
       return "Competencia no encontrada";
   }
 };
+
+
 </script>
--- src/views/InterventionsView.vue
+++ src/views/InterventionsView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full overflow-x-auto">
-    <h1 class="text-4xl font-bold mt-16 mb-4 ml-10">Lista de Intervenciones</h1>
+    <h1 class="text-4xl font-bold mt-16 mb-4 ml-10">Intervenciones</h1>
 
     <div class="grid place-items-center mb-5">
       <span class="font-bold text-2xl mb-2">Filtros</span>
@@ -24,14 +24,12 @@
     </div>
 
     <div class="text-center mb-6">
-      <span class="text-lg font-semibold"
-        >Total de Intervenciones: {{ filteredInterventions.length }}</span
-      >
+      <span class="text-lg font-semibold">
+        Total de Intervenciones: {{ filteredInterventions.length }}
+      </span>
     </div>
 
-    <div
-      class="grid place-items-center grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-y-10 mb-16 px-16"
-    >
+    <div class="grid place-items-center grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-y-10 mb-16 px-16">
       <InterventionCard
         v-for="(el, index) in filteredInterventions"
         :data="el"
@@ -42,399 +40,22 @@
 </template>
 
 <script setup>
-import { ref, computed } from "vue";
+import { ref, computed, onMounted } from "vue";
 import { Badge } from "@/components/ui/badge";
+import { api } from "@/api";
 import FilterIcon from "@/components/icons/FilterIcon.vue";
 import InterventionCard from "@/components/InterventionCard.vue";
 
-const interventions = [
-  {
-    id: 1,
-    type: "Conferencia",
-    intervened_competency: [
-      "adaptability_to_change",
-      "safe_conduct",
-      "personal_effectiveness",
-    ],
-    details: "Mejora de habilidades blandas",
-    duration_days: 4,
-  },
-  {
-    id: 2,
-    type: "Curso",
-    intervened_competency: ["adaptability_to_change", "personal_effectiveness"],
-    details: "Desarrollo de competencias técnicas",
-    duration_days: 4,
-  },
-  {
-    id: 3,
-    type: "Conferencia",
-    intervened_competency: ["dynamism_energy", "personal_effectiveness"],
-    details: "Refuerzo de la cultura organizacional",
-    duration_days: 4,
-  },
-  {
-    id: 4,
-    type: "Taller",
-    intervened_competency: ["safe_conduct", "initiative"],
-    details: "Mejora de habilidades blandas",
-    duration_days: 3,
-  },
-  {
-    id: 5,
-    type: "Conferencia",
-    intervened_competency: ["safe_conduct", "working_under_pressure"],
-    details: "Mejora de habilidades blandas",
-    duration_days: 1,
-  },
-  {
-    id: 6,
-    type: "Sesión práctica",
-    intervened_competency: ["dynamism_energy", "personal_effectiveness"],
-    details: "Refuerzo de la cultura organizacional",
-    duration_days: 4,
-  },
-  {
-    id: 7,
-    type: "Conferencia",
-    intervened_competency: ["adaptability_to_change", "personal_effectiveness"],
-    details: "Desarrollo de competencias técnicas",
-    duration_days: 5,
-  },
-  {
-    id: 8,
-    type: "Conferencia",
-    intervened_competency: ["adaptability_to_change", "initiative"],
-    details: "Refuerzo de la cultura organizacional",
-    duration_days: 3,
-  },
-  {
-    id: 9,
-    type: "Sesión práctica",
-    intervened_competency: ["adaptability_to_change", "initiative"],
-    details: "Refuerzo de la cultura organizacional",
-    duration_days: 1,
-  },
-  {
-    id: 10,
-    type: "Taller",
-    intervened_competency: ["adaptability_to_change", "initiative"],
-    details: "Mejora de habilidades blandas",
-    duration_days: 2,
-  },
-  {
-    id: 11,
-    type: "Simulación VR",
-    intervened_competency: ["adaptability_to_change", "safe_conduct"],
-    details: "Desarrollo de competencias técnicas",
-    duration_days: 3,
-  },
-  {
-    id: 12,
-    type: "Sesión práctica",
-    intervened_competency: ["adaptability_to_change", "initiative"],
-    details: "Desarrollo de competencias técnicas",
-    duration_days: 3,
-  },
-  {
-    id: 13,
-    type: "Conferencia",
-    intervened_competency: [
-      "dynamism_energy",
-      "initiative",
-      "working_under_pressure",
-    ],
-    details: "Desarrollo de competencias técnicas",
-    duration_days: 2,
-  },
-  {
-    id: 14,
-    type: "Taller",
-    intervened_competency: [
-      "adaptability_to_change",
-      "safe_conduct",
-      "personal_effectiveness",
-    ],
-    details: "Entrenamiento específico para situaciones críticas",
-    duration_days: 2,
-  },
-  {
-    id: 15,
-    type: "Curso",
-    intervened_competency: ["dynamism_energy", "personal_effectiveness"],
-    details: "Entrenamiento específico para situaciones críticas",
-    duration_days: 2,
-  },
-  {
-    id: 16,
-    type: "Taller",
-    intervened_competency: ["initiative", "working_under_pressure"],
-    details: "Mejora de habilidades blandas",
-    duration_days: 1,
-  },
-  {
-    id: 17,
-    type: "Sesión práctica",
-    intervened_competency: ["dynamism_energy", "working_under_pressure"],
-    details: "Entrenamiento específico para situaciones críticas",
-    duration_days: 5,
-  },
-  {
-    id: 18,
-    type: "Sesión práctica",
-    intervened_competency: [
-      "adaptability_to_change",
-      "safe_conduct",
-      "personal_effectiveness",
-    ],
-    details: "Refuerzo de la cultura organizacional",
-    duration_days: 1,
-  },
-  {
-    id: 19,
-    type: "Taller",
-    intervened_competency: ["dynamism_energy", "personal_effectiveness"],
-    details: "Mejora de habilidades blandas",
-    duration_days: 1,
-  },
-  {
-    id: 20,
-    type: "Sesión práctica",
-    intervened_competency: ["dynamism_energy", "personal_effectiveness"],
-    details: "Aplicación de nuevas metodologías",
-    duration_days: 3,
-  },
-  {
-    id: 21,
-    type: "Conferencia",
-    intervened_competency: ["adaptability_to_change", "safe_conduct"],
-    details: "Mejora de habilidades blandas",
-    duration_days: 3,
-  },
-  {
-    id: 22,
-    type: "Simulación VR",
-    intervened_competency: ["adaptability_to_change", "initiative"],
-    details: "Entrenamiento específico para situaciones críticas",
-    duration_days: 2,
-  },
-  {
-    id: 23,
-    type: "Curso",
-    intervened_competency: ["adaptability_to_change", "safe_conduct"],
-    details: "Aplicación de nuevas metodologías",
-    duration_days: 4,
-  },
-  {
-    id: 24,
-    type: "Curso",
-    intervened_competency: ["dynamism_energy", "personal_effectiveness"],
-    details: "Desarrollo de competencias técnicas",
-    duration_days: 4,
-  },
-  {
-    id: 25,
-    type: "Sesión práctica",
-    intervened_competency: ["adaptability_to_change", "initiative"],
-    details: "Refuerzo de la cultura organizacional",
-    duration_days: 5,
-  },
-  {
-    id: 26,
-    type: "Curso",
-    intervened_competency: ["dynamism_energy", "personal_effectiveness"],
-    details: "Aplicación de nuevas metodologías",
-    duration_days: 1,
-  },
-  {
-    id: 27,
-    type: "Curso",
-    intervened_competency: [
-      "adaptability_to_change",
-      "safe_conduct",
-      "personal_effectiveness",
-    ],
-    details: "Mejora de habilidades blandas",
-    duration_days: 4,
-  },
-  {
-    id: 28,
-    type: "Conferencia",
-    intervened_competency: ["adaptability_to_change", "initiative"],
-    details: "Mejora de habilidades blandas",
-    duration_days: 3,
-  },
-  {
-    id: 29,
-    type: "Simulación VR",
-    intervened_competency: ["initiative", "working_under_pressure"],
-    details: "Refuerzo de la cultura organizacional",
-    duration_days: 5,
-  },
-  {
-    id: 30,
-    type: "Sesión práctica",
-    intervened_competency: ["dynamism_energy", "personal_effectiveness"],
-    details: "Desarrollo de competencias técnicas",
-    duration_days: 3,
-  },
-  {
-    id: 31,
-    type: "Sesión práctica",
-    intervened_competency: [
-      "dynamism_energy",
-      "initiative",
-      "working_under_pressure",
-    ],
-    details: "Aplicación de nuevas metodologías",
-    duration_days: 4,
-  },
-  {
-    id: 32,
-    type: "Taller",
-    intervened_competency: ["initiative", "working_under_pressure"],
-    details: "Entrenamiento específico para situaciones críticas",
-    duration_days: 2,
-  },
-  {
-    id: 33,
-    type: "Simulación VR",
-    intervened_competency: ["adaptability_to_change", "initiative"],
-    details: "Entrenamiento específico para situaciones críticas",
-    duration_days: 3,
-  },
-  {
-    id: 34,
-    type: "Taller",
-    intervened_competency: ["dynamism_energy", "working_under_pressure"],
-    details: "Refuerzo de la cultura organizacional",
-    duration_days: 4,
-  },
-  {
-    id: 35,
-    type: "Taller",
-    intervened_competency: [
-      "adaptability_to_change",
-      "safe_conduct",
-      "personal_effectiveness",
-    ],
-    details: "Aplicación de nuevas metodologías",
-    duration_days: 3,
-  },
-  {
-    id: 36,
-    type: "Sesión práctica",
-    intervened_competency: ["dynamism_energy", "working_under_pressure"],
-    details: "Refuerzo de la cultura organizacional",
-    duration_days: 1,
-  },
-  {
-    id: 37,
-    type: "Conferencia",
-    intervened_competency: ["adaptability_to_change", "initiative"],
-    details: "Entrenamiento específico para situaciones críticas",
-    duration_days: 4,
-  },
-  {
-    id: 38,
-    type: "Simulación VR",
-    intervened_competency: ["dynamism_energy", "personal_effectiveness"],
-    details: "Mejora de habilidades blandas",
-    duration_days: 3,
-  },
-  {
-    id: 39,
-    type: "Taller",
-    intervened_competency: ["safe_conduct", "working_under_pressure"],
-    details: "Desarrollo de competencias técnicas",
-    duration_days: 5,
-  },
-  {
-    id: 40,
-    type: "Sesión práctica",
-    intervened_competency: [
-      "adaptability_to_change",
-      "safe_conduct",
-      "personal_effectiveness",
-    ],
-    details: "Aplicación de nuevas metodologías",
-    duration_days: 5,
-  },
-  {
-    id: 41,
-    type: "Conferencia",
-    intervened_competency: ["initiative", "working_under_pressure"],
-    details: "Entrenamiento específico para situaciones críticas",
-    duration_days: 5,
-  },
-  {
-    id: 42,
-    type: "Sesión práctica",
-    intervened_competency: ["initiative", "working_under_pressure"],
-    details: "Aplicación de nuevas metodologías",
-    duration_days: 1,
-  },
-  {
-    id: 43,
-    type: "Curso",
-    intervened_competency: [
-      "dynamism_energy",
-      "safe_conduct",
-      "personal_effectiveness",
-    ],
-    details: "Desarrollo de competencias técnicas",
-    duration_days: 5,
-  },
-  {
-    id: 44,
-    type: "Simulación VR",
-    intervened_competency: ["dynamism_energy", "personal_effectiveness"],
-    details: "Desarrollo de competencias técnicas",
-    duration_days: 3,
-  },
-  {
-    id: 45,
-    type: "Simulación VR",
-    intervened_competency: ["dynamism_energy", "personal_effectiveness"],
-    details: "Aplicación de nuevas metodologías",
-    duration_days: 1,
-  },
-  {
-    id: 46,
-    type: "Sesión práctica",
-    intervened_competency: ["initiative", "working_under_pressure"],
-    details: "Mejora de habilidades blandas",
-    duration_days: 3,
-  },
-  {
-    id: 47,
-    type: "Sesión práctica",
-    intervened_competency: ["adaptability_to_change", "safe_conduct"],
-    details: "Mejora de habilidades blandas",
-    duration_days: 2,
-  },
-  {
-    id: 48,
-    type: "Simulación VR",
-    intervened_competency: ["adaptability_to_change", "personal_effectiveness"],
-    details: "Desarrollo de competencias técnicas",
-    duration_days: 2,
-  },
-  {
-    id: 49,
-    type: "Taller",
-    intervened_competency: ["dynamism_energy", "personal_effectiveness"],
-    details: "Mejora de habilidades blandas",
-    duration_days: 1,
-  },
-  {
-    id: 50,
-    type: "Curso",
-    intervened_competency: ["safe_conduct", "working_under_pressure"],
-    details: "Refuerzo de la cultura organizacional",
-    duration_days: 1,
-  },
-];
+const interventions = ref([]);
+
+const fetchInterventions = async () => {
+  try {
+    const response = await api.get("/interventions-types");
+    interventions.value = response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 const translateCompetency = (competency) => {
   switch (competency) {
@@ -476,16 +97,16 @@ const toggleCompetency = (competency) => {
 
 const filteredInterventions = computed(() => {
   if (selectedCompetencies.value.size === 0) {
-    return interventions;
+    return interventions.value;
   }
-  return interventions.filter((intervention) =>
-    intervention.intervened_competency
+  return interventions.value.filter((intervention) =>
+    intervention.intervened_competencies
       .map(translateCompetency)
       .some((competency) => selectedCompetencies.value.has(competency))
   );
 });
-</script>
 
-<style scoped>
-/* Puedes agregar estilos adicionales aquí */
-</style>
+onMounted(() => {
+  fetchInterventions();
+});
+</script>
